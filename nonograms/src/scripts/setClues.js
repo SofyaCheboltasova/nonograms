fa1302bb3dab31f8eed6dd93bbee7d232b1b39cc
Sofya Cheboltasova <s.cheboltasova@g.nsu.ru>
2024-01-30 20:40:27 +0700

    feat, refactor: implement clues filling, refactor styles for clues background

--- nonograms/src/scripts/setClues.js
+++ nonograms/src/scripts/setClues.js
@@ -1,9 +1,20 @@
-/* eslint-disable no-console */
-function setClues(nonogram) {
-  const { metadata } = nonogram;
-  const { rows, columns } = metadata;
+function setCluesForLine(cell, line) {
+  for (let j = 0; j < line.length; j += 1) {
+    const span = document.createElement("span");
+    span.textContent = `${line[j]}`;
+    cell.appendChild(span);
+  }
+}
 
-  console.error(rows, columns);
+function setClues(nonogram) {
+  const { size, metadata } = nonogram;
+  const { rows, columns } = metadata;
+  const cells = document.querySelectorAll(".cell_clue");
+
+  for (let i = 0; i < size; i += 1) {
+    setCluesForLine(cells[i + 1], columns[i]);
+    setCluesForLine(cells[(i + 1) * (size + 1)], rows[i]);
+  }
 }
 
 export default setClues;
