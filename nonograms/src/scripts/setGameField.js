b514f097cf42587c29a08492b677831fc662df0c
Sofya Cheboltasova <s.cheboltasova@g.nsu.ru>
2024-01-30 17:28:51 +0700

    feat, refactor: set template and styles for clues, refactor styles for gameField

--- nonograms/src/scripts/setGameField.js
+++ nonograms/src/scripts/setGameField.js
@@ -12,9 +12,38 @@ function createCellsLine(size, row) {
   return tr;
 }
 
+function setClues(size) {
+  const cluesField = document.createElement("div");
+  cluesField.classList.add("gameField__clues");
+
+  const cluesTop = document.createElement("div");
+  cluesTop.classList.add("gameField__clues_top");
+
+  const cluesLeft = document.createElement("div");
+  cluesLeft.classList.add("gameField__clues_left");
+
+  for (let i = 0; i < size + 1; i += 1) {
+    const divider = document.createElement("div");
+    divider.classList.add("divider-vertical");
+    cluesTop.appendChild(divider);
+  }
+
+  for (let i = 0; i < size + 1; i += 1) {
+    const divider = document.createElement("div");
+    divider.classList.add("divider-horizontal");
+    cluesLeft.appendChild(divider);
+  }
+
+  cluesField.append(cluesTop, cluesLeft);
+
+  return cluesField;
+}
+
 function setGameField(size) {
   const gameField = document.createElement("div");
   gameField.classList.add("gameField");
+
+  const cluesField = setClues(size);
 
   const cellsField = document.createElement("table");
   cellsField.classList.add("gameField__cells");
@@ -23,7 +52,7 @@ function setGameField(size) {
     const cellLine = createCellsLine(size, i);
     cellsField.append(cellLine);
   }
-  gameField.appendChild(cellsField);
+  gameField.append(cluesField, cellsField);
   return gameField;
 }
 
